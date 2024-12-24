91a01f7cfa606ba151b042afe0cc0355a7761e03
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-23 22:36:35 -0300

    react

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,20 @@
 function Greeting() {
-    return <h1>&quot;Hello, World!!!&quot;</h1>
+    // return <h1>&quot;If you don't try to save one life, you'll never save any&quot;</h1>
+    return ( 
+    <>
+    <h1>Hedy Lamarr's Todos</h1>
+    <img 
+      src="https://i.imgur.com/yXOvdOSs.jpg" 
+      alt="Hedy Lamarr" 
+      className="photo" 
+    />
+    <ul>
+      <li>Invent new traffic lights</li>
+      <li>Rehearse a movie scene</li>
+      <li>Improve the spectrum technology</li>
+    </ul>
+  </>
+    )
 }
 
 export default Greeting
