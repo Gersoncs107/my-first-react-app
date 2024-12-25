508af578f0c4807cc16b9d75db2fc4cbfdd059c1
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-24 22:17:39 -0300

    gitAdd

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -2,13 +2,22 @@ function Greeting() {
     // return <h1>&quot;If you don't try to save one life, you'll never save any&quot;</h1>
     return ( 
     <>
-    <h1>Hedy Lamarr's Todos</h1>
+    <h1>Pepe's Todos</h1>
     <img 
-      src="https://i.imgur.com/yXOvdOSs.jpg" 
-      alt="Hedy Lamarr" 
+      src="https://i.imgur.com/segHJlQ.jpeg" 
+      alt="Sad Pepe" 
       className="photo" 
     />
-    <ul>
+    <br/>
+    <input type="text" />
+    <br/>
+    <button style={{
+      backgroundColor:"aquamarine"
+    }}>Submit</button>
+    <ul style={{
+      backgroundColor: "black",
+      color: "red"
+    }}>
       <li>Invent new traffic lights</li>
       <li>Rehearse a movie scene</li>
       <li>Improve the spectrum technology</li>
