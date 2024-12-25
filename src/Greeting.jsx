fe02e6c04075dc5dbf268696fea4a24d45b7c826
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-24 22:25:20 -0300

    react

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -10,7 +10,7 @@ function Greeting() {
     // return <h1>&quot;If you don't try to save one life, you'll never save any&quot;</h1>
     return ( 
     <>
-    <h1>{name}'s Todos</h1>
+    <h1>{person.name}'s Todos</h1>
     <img 
       src="https://i.imgur.com/segHJlQ.jpeg" 
       alt="Sad Pepe" 
