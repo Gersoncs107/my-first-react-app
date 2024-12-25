b919825afe8330963a3e6b08622c7da30cb8a6f3
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-24 22:30:42 -0300

    react

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -9,7 +9,7 @@ const person = {
 function Greeting() {
     // return <h1>&quot;If you don't try to save one life, you'll never save any&quot;</h1>
     return ( 
-    <div style={{}}>
+    <div style={{backgroundColor:"black", color: "red"}}>
     <h1>{person.name}'s Todos</h1>
     <img 
       src="https://i.imgur.com/segHJlQ.jpeg" 
