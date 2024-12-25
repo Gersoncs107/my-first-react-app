ffd55900cced7f1a4e5324807d086c0d215ff34b
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-24 22:21:33 -0300

    react

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,8 +1,10 @@
+const name = "Sad Pepe"
+
 function Greeting() {
     // return <h1>&quot;If you don't try to save one life, you'll never save any&quot;</h1>
     return ( 
     <>
-    <h1>Pepe's Todos</h1>
+    <h1>{name}'s Todos</h1>
     <img 
       src="https://i.imgur.com/segHJlQ.jpeg" 
       alt="Sad Pepe" 
