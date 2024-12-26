48cb3ce2bbea8e98e63178309e858896fb07ac74
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-26 19:33:55 -0300

    react

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -22,32 +22,32 @@ function Animal(){
   )
 }
 
-function Greeting() {
-    // return <h1>&quot;If you don't try to save one life, you'll never save any&quot;</h1>
-    return ( 
-    <div style={{backgroundColor:"black", color: "purple", width: "600px"}}>
-    <h1>{person.name}'s Todos</h1>
-    <img style={{
-      borderRadius:"10px"
-    }}
-      src="https://i.imgur.com/segHJlQ.jpeg" 
-      alt="Sad Pepe" 
-      className="photo" 
-    />
-    <br/>
-    <input type="text" />
-    <br/>
-    <button style={{
-      backgroundColor:"aquamarine",
-      borderRadius: "10px"
-    }}>Submit</button>
-    <ul>
-      <li>Invent new traffic lights</li>
-      <li>Rehearse a movie scene</li>
-      <li>Improve the spectrum technology</li>
-    </ul>
-  </div>
-    )
-}
+// function Greeting() {
+//     // return <h1>&quot;If you don't try to save one life, you'll never save any&quot;</h1>
+//     return ( 
+//     <div style={{backgroundColor:"black", color: "purple", width: "600px"}}>
+//     <h1>{person.name}'s Todos</h1>
+//     <img style={{
+//       borderRadius:"10px"
+//     }}
+//       src="https://i.imgur.com/segHJlQ.jpeg" 
+//       alt="Sad Pepe" 
+//       className="photo" 
+//     />
+//     <br/>
+//     <input type="text" />
+//     <br/>
+//     <button style={{
+//       backgroundColor:"aquamarine",
+//       borderRadius: "10px"
+//     }}>Submit</button>
+//     <ul>
+//       <li>Invent new traffic lights</li>
+//       <li>Rehearse a movie scene</li>
+//       <li>Improve the spectrum technology</li>
+//     </ul>
+//   </div>
+//     )
+// }
 
-export default Greeting
+export default Animal
