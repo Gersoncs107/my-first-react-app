a3ffcc9519efaebb66952a0bd6d342a8ea5d9127
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-28 22:02:18 -0300

    react

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -7,7 +7,9 @@ const todos = [
 export function TodoList(){
      return(
         <ul>
-            
+            {todos.map( todo => {
+                return <li key={todo.id}>{todo.task}</li>
+            })}
         </ul>
      )
 }
