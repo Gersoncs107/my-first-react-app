7dbe7ebd3cfbe79943b4dc97bb789f691988039c
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-28 21:58:52 -0300

    react

---
+++ src/Todo.jsx
@@ -0,0 +1,6 @@
+const todos = [
+    {task: "mow yard", id: crypto.randomUUID()},
+    {task: "Work on Odin Projects", id: crypto.randomUUID()},
+    {task: "Feed the cat", id: crypto.randomUUID()}
+]
+
