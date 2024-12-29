20fee104e5b700e8e5553eff273385172f122f18
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-28 22:05:34 -0300

    react

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
+import TodoList from './Todo.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
+    <TodoList />
   </StrictMode>,
 )
