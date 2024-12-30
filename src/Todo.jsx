74022281c6c611728e9234910992ecfe2d83bbee
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-29 22:15:18 -0300

    react

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,6 +4,8 @@ const todos = [
     {task: "Feed the cat", id: crypto.randomUUID()}
 ]
 
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
 export default function TodoList(){
      return(
      <div style={{color: "blue", backgroundColor:"aquamarine", width: "500px", height: "400px"}}>
