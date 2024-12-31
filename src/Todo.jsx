ebf1e84417878157b895433cdc33536d67d35b46
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-31 19:32:07 -0300

    react

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,3 +3,13 @@
         <button>Click Me!</button>
     )
  }
+
+ export function AppButton(){
+    return(
+    <div>
+        <Button/>
+        <Button/>
+        <Button/>
+    </div>
+    )
+ }
