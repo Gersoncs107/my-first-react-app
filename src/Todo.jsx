e6c1e74edaa563072a3e9bf44de7ac286ba502a1
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-31 19:51:50 -0300

    react

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,14 +4,14 @@
         fontSize: props.fontSize
     }
     return (
-        <button style={buttonStyle}>props.text</button>
+        <button style={buttonStyle}>{props.text}</button>
     )
  }
 
  export default function AppButton(){
     return(
     <div>
-        <Button/>
+        <Button text= "Click Me!"color="blue" fontSize={12}/>
         <Button/>
         <Button/>
     </div>
