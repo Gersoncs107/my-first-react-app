7ca54a71ad184bfc63eee29d15f6b732d688d656
Gersoncs107 <gerson.silva107@gmail.com>
2024-12-31 19:29:17 -0300

    react

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,19 +1,5 @@
-const todos = [
-    {task: "mow yard", id: crypto.randomUUID()},
-    {task: "Work on Odin Projects", id: crypto.randomUUID()},
-    {task: "Feed the cat", id: crypto.randomUUID()}
-]
-
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-export default function TodoList(){
-     return(
-     <div style={{color: "blue", backgroundColor:"aquamarine", width: "500px", height: "400px"}}>
-        <ul style={{textAlign: "left"}}>
-            {months.map( (month, index) => {
-               return <li key={index}>{month}</li>
-            })}
-        </ul>
-     </div>
-     )
-}
+ function Button(){
+    return (
+        <button>Click Me!</button>
+    )
+ }
