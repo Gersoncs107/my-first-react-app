8176d1e4ae39351bd619df56f8f85f89a37b0f59
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-02 20:01:15 -0300

    react

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -8,10 +8,16 @@
     )
  }
 
+ Button.defaultProps = {
+    text:"Click Me!",
+    color: "blue",
+    fontSize: 12
+ }
+
  export default function AppButton(){
     return(
     <div>
-        <Button text="Click Me!" color="blue" fontSize={12}/>
+        <Button/>
         <Button text="Don't Click Me!" color= "red" fontSize={12}/>
         <Button text="Click Me!" color="blue" fontSize={20}/>
     </div>
