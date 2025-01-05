245a53d74f5a6369808e78ffe8329fee757ebd17
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-04 22:30:58 -0300

    react

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -24,7 +24,7 @@
         window.location.href = url
     }
     return(
-    <div style={{backgroundColor: "rgb(23, 151, 98)"}}>
+    <div style={{backgroundColor: "rgb(23, 151, 98)", width: "400px"}}>
         <Button handleClick={() => handleButtonClick("https://www.google.com")} />
     </div>
     )
