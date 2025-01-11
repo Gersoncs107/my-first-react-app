8b53ea5f629e9847b026eaf1b789260e9ac4c216
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-11 19:33:53 -0300

    react

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 export default function Clock(){
     const [count, setCount] = useState(0)
 
-    
+    setInterval(() => {
+        setCount(count => count + 1)
+    }, 1000);
 
 }
