957981436616866793989027348ddca2204c81fe
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-18 19:50:09 -0300

    react

--- ClassComponents.jsx
+++ ClassComponents.jsx
@@ -9,7 +9,13 @@ class ClassInput extends Component{
         return (
             <section>
                 <h3>{this.props.name}</h3>
-                
+                <form >
+                    <label htmlFor="task-entry">Enter a task</label>
+                    <input type="text" name="task-entry" />
+                    <button type="submit">Submit</button>
+                </form>
+                <h4>All the tasks</h4>
+                <ul></ul>
             </section>
         )
     }
