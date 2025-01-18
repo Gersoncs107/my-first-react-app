4036be48c8645dd8137f0bf7c4118c83a825370e
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-18 20:00:00 -0300

    react

--- ClassComponents.jsx
+++ ClassComponents.jsx
@@ -8,15 +8,18 @@ class ClassInput extends Component{
             todos: [],
             inputVal: "",
           };
+
+          this.handleInputChange = this.handleInputChange.bind(this)
+          this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     render() {
         return (
             <section>
                 <h3>{this.props.name}</h3>
-                <form >
+                <form onSubmit={this.handleSubmit}>
                     <label htmlFor="task-entry">Enter a task</label>
-                    <input type="text" name="task-entry" />
+                    <input type="text" name="task-entry" onChange={this.handleInputChange}/>
                     <button type="submit">Submit</button>
                 </form>
                 <h4>All the tasks</h4>
