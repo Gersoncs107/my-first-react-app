f47d2317b3f70372ab8a82e8452211559b4ee499
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-17 22:30:43 -0300

    react

--- src/ClassBased.jsx
+++ src/ClassBased.jsx
@@ -3,4 +3,8 @@ import { useState } from "react";
 export default function FunctionalInput({_name}){
     const [todo, setTodo] = useState(["Just some demo tasks", "As an example"])
     const [inputVal, setInputVal] = useState("")
+
+    const handleInputChange = (e) => {
+        setInputVal(e.target.value)
+    }
 }
