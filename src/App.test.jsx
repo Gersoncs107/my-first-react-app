a9e3011f02d10c6b198cab0f706cf5f87900778f
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-23 19:21:42 -0300

    react

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,7 @@
 import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
 
 describe('something truthy and falsy', () => {
     it('true to be true', () => {
