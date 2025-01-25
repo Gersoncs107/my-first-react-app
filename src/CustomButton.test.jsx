8cee6a1acfb878026964b6fcc4df8d3b563ae477
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-24 22:13:48 -0300

    react

--- src/CustomButton.test.jsx
+++ src/CustomButton.test.jsx
@@ -3,3 +3,8 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CustomButton from "./CustomButton";
 
+describe("CustomButton", ()=> {
+    it("should render a button with the text 'Click me'", () => {
+        render(<CustomButton onClick={ () => {}} />)
+    })
+})
