1bf4726b78922dbaf9718b9457a565a344e795eb
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-24 22:06:02 -0300

    react

--- src/CustomButton.test.jsx
+++ src/CustomButton.test.jsx
@@ -1,3 +1,5 @@
 import { vi, describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import CustomButton from "./CustomButton";
+
